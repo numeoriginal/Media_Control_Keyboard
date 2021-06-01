cd574fdc96aafcfa1cd0d3db6ceb1cf8188b5ff3
Constantin Carp <44733300+numeoriginal@users.noreply.github.com>
2021-06-01 13:29:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,23 @@ arduino.flushOutput()
 pause = 0
 play = 0
 
+def map_volume(value, left_min, left_max, right_min, right_max):
+    left_span = left_max - left_min
+    right_span = right_max - right_min
+
+    value_scaled = float(value - left_min) / float(left_span)
+
+    return right_min + (value_scaled * right_span)
+
+
+def volume_set(value):
+    global last_volume
+    map_value = map_volume(value, 0, 100, 0, 65535)
+    print(f'Current system volume: {value}')
+    if last_volume != map_value:
+        os.system("nircmd.exe setsysvolume %d" % map_value)
+        last_volume = map_value
+
 
 def read():
     global pause
@@ -16,14 +33,6 @@ def read():
     data = arduino.readline().decode('utf-8').rstrip()
 
     if data:
-        if data == "Up":
-            keyboard.press('volumeup')
-            pause = 0
-            play = 0
-        if data == "Down":
-            keyboard.press('volumedown')
-            pause = 0
-            play = 0
         if data == "prev":
             keyboard.press('prevtrack')
             pause = 0
@@ -52,6 +61,8 @@ def read():
             keyboard.press('win')
             pause = 0
             play = 0
+        if data.isnumeric():
+            volume_set(int(data))
 
 
 if __name__ == '__main__':
